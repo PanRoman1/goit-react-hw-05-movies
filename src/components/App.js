9d813f156eb4d22e95f1c7ccb3e969c7faab23c7
PanRoman1 <panchuk1985@ukr.net>
2023-01-26 16:56:12 +0200

    HW5

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,14 @@
+import { Route, Routes } from 'react-router-dom';
 import { Layout } from './Layout';
 
 export const App = () => {
-  return <Layout>React</Layout>;
+  return (
+    <Layout>
+      <Routes>
+        <Route path="/" element={< />} />
+        <Route path="/about" element={< />} />
+        <Route path="/products" element={< />} />
+      </Routes>
+    </Layout>
+  );
 };
--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import { GlobalStyle } from 'components/GlobalStyle';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-    <GlobalStyle />
+    <BrowserRouter>
+      <App />
+      <GlobalStyle />
+    </BrowserRouter>
   </React.StrictMode>
 );
