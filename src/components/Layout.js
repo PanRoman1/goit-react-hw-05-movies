3d7df11c9e2aca7de16c8e15f8154b7d242815e7
PanRoman1 <panchuk1985@ukr.net>
2023-01-26 16:31:44 +0200

    HW5

---
+++ src/components/Layout.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Layout = styled.section`
+  margin: 24px;
+  padding: 8px;
+  border: 1px solid lightgray;
+  border-radius: 4px;
+  width: max-content;
+`;
